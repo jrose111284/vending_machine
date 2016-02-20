5047653873362ee5fdc6e27a0409aec7c0317ab5
jamierose <jrose111284@gmail.com>
2016-02-20 19:03:48 +0000

    refactored with attr_reader for display

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,13 +1,11 @@
 class VendingMachine
+  attr_reader :display
+
   def initialize
     @display = 'INSERT COIN'
   end
 
   def insert coin
     @display = '5 cents'
-  end
-
-  def display
-    @display
   end
 end
