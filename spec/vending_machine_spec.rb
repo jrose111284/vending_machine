c6a290793d332979aa254d76eb3c3e27b0acdbe7
jamie rose <jrose111284@gmail.com>
2016-02-22 16:13:06 +0000

    can add conbo of coins into machine

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -17,6 +17,13 @@ context "accepts coins" do
     subject.insert '25'
     expect(subject.display).to eq '25 cents'
   end
+
+  it 'accepts coin combo and updates display' do
+    subject.insert '5'
+    subject.insert '10'
+    subject.insert '25'
+    expect(subject.display).to eq '40 cents'
+  end
 end
 
 context 'invalid coins' do
