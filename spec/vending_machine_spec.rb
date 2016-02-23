610385ebb4bf8524692200f5f1e307b0370684c5
jamierose <jrose111284@gmail.com>
2016-02-23 10:17:02 +0000

    refactored

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,46 +1,45 @@
 require 'vending_machine'
 
 describe VendingMachine do
+  context 'accepts coins' do
+    it 'accepts a nickel and updates display' do
+      subject.insert '5'
+      expect(subject.display).to eq '5 cents'
+    end
 
-context "accepts coins" do
-  it 'accepts a nickel and updates display' do
-    subject.insert '5'
-    expect(subject.display).to eq '5 cents'
+    it 'accepts dime and updates display' do
+      subject.insert '10'
+      expect(subject.display).to eq '10 cents'
+    end
+
+    it 'accepts quarter and updates display' do
+      subject.insert '25'
+      expect(subject.display).to eq '25 cents'
+    end
+
+    it 'accepts coin combo and updates display' do
+      subject.insert '5'
+      subject.insert '10'
+      subject.insert '25'
+      expect(subject.display).to eq '40 cents'
+    end
   end
 
-  it 'accepts dime and updates display' do
-    subject.insert '10'
-    expect(subject.display).to eq '10 cents'
+  context 'invalid coins' do
+    it 'rejects a penny and updates display' do
+      subject.insert '1'
+      expect(subject.display).to eq 'INSERT COIN'
+      expect(subject.coin_return).to eq '1'
+    end
+
+    it 'rejects a non-coin and updates display' do
+      subject.insert '1wedwd3'
+      expect(subject.display).to eq 'INSERT COIN'
+      expect(subject.coin_return).to eq '1wedwd3'
+    end
   end
 
-  it 'accepts quarter and updates display' do
-    subject.insert '25'
-    expect(subject.display).to eq '25 cents'
-  end
-
-  it 'accepts coin combo and updates display' do
-    subject.insert '5'
-    subject.insert '10'
-    subject.insert '25'
-    expect(subject.display).to eq '40 cents'
-  end
-end
-
-context 'invalid coins' do
-  it 'rejects a penny and updates display' do
-    subject.insert '1'
-    expect(subject.display).to eq 'INSERT COIN'
-    expect(subject.coin_return).to eq '1'
-  end
-
-  it 'rejects a non-coin and updates display' do
-    subject.insert '1wedwd3'
-    expect(subject.display).to eq 'INSERT COIN'
-    expect(subject.coin_return).to eq '1wedwd3'
-  end
- end
-
-  it "displays INSERT COIN when no coins inserted" do
+  it 'displays INSERT COIN when no coins inserted' do
     expect(subject.display).to eq('INSERT COIN')
   end
 end
