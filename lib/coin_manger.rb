a343728a9cbd80abf414e4c2c924eee97db2a872
jamierose <jrose111284@gmail.com>
2016-03-06 18:12:43 +0000

    started to move coins and total to coin manger class

--- lib/coin_manger.rb
+++ lib/coin_manger.rb
@@ -1,2 +1,18 @@
 class CoinManger
+  VALID_COINS = %w(5 10 25)
+
+  def initialize
+    self.coins = []
+  end
+
+  def total
+    coins.map(&:to_i).inject(:+) || 0
+  end
+
+  def insert coin
+    coins.push(coin)  if VALID_COINS.include? coin
+  end
+
+  private
+  attr_accessor :coins
 end
--- spec/coin_manger_spec.rb
+++ spec/coin_manger_spec.rb
@@ -1,4 +1,44 @@
 require 'coin_manger'
 describe CoinManger do
 
+  it "has zero total by deafult" do
+    expect(subject.total).to eq(0)
+  end
+
+  context 'accepts coins' do
+    it 'accepts a nickel and updates total' do
+      subject.insert '5'
+      expect(subject.total).to eq 5
+    end
+
+    it 'accepts dime and updates total' do
+      subject.insert '10'
+      expect(subject.total).to eq 10
+    end
+
+    it 'accepts quarter and updates total' do
+      subject.insert '25'
+      expect(subject.total).to eq 25
+    end
+
+    it 'accepts coin combo and updates total' do
+      subject.insert '5'
+      subject.insert '10'
+      subject.insert '25'
+      expect(subject.total).to eq 40
+    end
+  end
+
+  context 'invalid coins' do
+    it 'rejects a penny and updates display' do
+      subject.insert '1'
+      expect(subject.total).to eq 0
+    end
+
+    it 'rejects a non-coin and updates display' do
+      subject.insert '1wedwd3'
+      expect(subject.total).to eq 0
+    end
+  end
+
 end
